48aecc92febb2b913cd336b968f76cfe4ac0072b
Tom Stian Ingebretson <toms.ingebretson@gmail.com>
2025-06-23 11:02:05 +0200

    fix: Only load in package.json files

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -3,7 +3,14 @@ return {
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 	},
+	ft = "json",
 	config = function()
 		require("package-info").setup()
+	end,
+	init = function()
+		local filename = vim.fn.expand("%:t")
+		if filename == "package.json" then
+			return true
+		end
 	end,
 }
