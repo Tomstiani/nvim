8983ddad1ef64fb5a7b5d27714e42ee54788b93b
Tom Stian Ingebretson <toms.ingebretson@gmail.com>
2025-07-11 14:55:11 +0200

    fix: More key stuff

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,9 +4,6 @@ local keymap = vim.keymap.set
 
 keymap("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open file explorer" })
 
--- Open CodeCompanion
-keymap("n", "<leader>c", "<CMD>CodeCompanionChat<CR>", { desc = "Open CodeCompanion" })
-
 -- Quickfix list
 keymap("n", "<leader>qq", "<CMD>copen<CR>", { desc = "Open quickfix list" })
 keymap("n", "<leader>qd", "<CMD>call setqflist([])<CR>", { desc = "Delete all quickfix entries" })
--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -5,6 +5,12 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		"ravitemer/mcphub.nvim",
 	},
+
+	keys = {
+		{ "<leader>cc", "<cmd>CodeCompanionChat toggle<cr>", { desc = "Toggle CodeCompanionChat" } },
+		{ "<leader>ca", "<cmd>CodeCompanionAction", { desc = "CodeCompanionAction" } },
+	},
+
 	config = function()
 		require("codecompanion").setup({
 			adapters = {
@@ -18,6 +24,33 @@ return {
 						},
 					})
 				end,
+				anthropic = function()
+					return require("codecompanion.adapters").extend("anthropic", {
+						name = "anthropic",
+					})
+				end,
+			},
+
+			strategies = {
+				chat = {
+					adapter = "ollama",
+				},
+				inline = {
+					adapter = "ollama",
+					keymaps = {
+						accept_change = {
+							modes = { n = "ca" },
+							description = "Accept the change",
+						},
+						reject_change = {
+							modes = { n = "cr" },
+							description = "Reject the change",
+						},
+					},
+				},
+				cmd = {
+					adapter = "ollama",
+				},
 			},
 
 			extensions = {
